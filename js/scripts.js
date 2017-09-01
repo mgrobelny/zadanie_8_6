faaed26a32c6700c62523240fcc3f83952e103e1
mgrobelny <michalmaciejg@gmail.com>
2017-09-01 18:41:13 +0200

    intial

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var a = 12
+	b = 3
+	value = (a * a) + (2 * a * b) - (b * b)
+
+console.log('Wynik wynosi: ' + value)
+
+if (value > 0) {
+	console.log('Wynik jest dodatni')
+} else {
+	console.log('Wynik jest ujemny')
+}
+
+if (value == 0) {
+	console.log('Wynik jest równy 0')
+}
